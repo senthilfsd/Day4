73656a3d02bbae6f0189b2bacdfba7cb59b0207c
senthilfsd <senthilgis.89@gmail.com>
2024-03-02 15:36:45 +0530

    for in update

--- JSON.js
+++ JSON.js
@@ -10,14 +10,14 @@ let myresume = [
 	    "name": "SENTHIL KUMAR",
 	    "Contactno": 9655542669,
 	    "emailid": "senthilgis.89@gmail.com",
-	},
+		},
 	{
 	    "education": "Eeducational Details",
 	    "pg": "MSc CS - The Gandhigram Rural University - Dindigul", 
 	    "ug": "BSc CS - Madurai Kamaraj University - Madurai", 
 	    "HSC": "Mathematics Computer - Government Higher Secondry School",
         "SSLC": "StateBoard - Government High School"
-	},
+		},
 	{
 	    "experience": "Experience details",  
 	    "totalexperience": 9, 
@@ -25,22 +25,28 @@ let myresume = [
         "gis engineer 2015 - 2016": "Genesys International Corporation Ltd - Bangalore", 
         "gis analyst 2016-2023": "Viana Technologies Pvt Ltd",
         "gis analyst 2023-present": " IdeaForge Technology-Bangalore" 
-	},
+		},
     {
         "technical skill": "Software Skills",
         "gis skill": " ArcGIS, AutoCAD, QGIS, Metashape",
         "coding skill": "HTML, CSS, JavaScript, Bootstrab" 
-    },
+  		},
     {
         "address": "P Thiruvengedapuram, Sivakasi, Virudhunagar, Tmilnadu",
 
-    }
+    	},
 ];
 
 	/*let resume = JSON.stringify(myresume);
 	console.log(resume);
 	console.log(typeof(resume));*/
+
+//...................For of .................................//	
     for(let resume of myresume){
         console.log(resume);
     }
 
+//..................For In .................................//	
+	for(let resume in myresume){
+		console.log(resume);
+	}
